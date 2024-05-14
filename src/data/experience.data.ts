0db2285901e728c13157088f2f8fd669289763a5
Erick Casanova <erickm.casanova@gmail.com>
2024-05-14 12:35:47 -0600

    add experience en

--- src/data/experience.data.ts
+++ src/data/experience.data.ts
@@ -7,49 +7,39 @@ export const EXPERIENCES_ES = [
   },
   {
     date: "",
-    title: "Super Tiendas Rarra's S.A. DE C.V. - Web Developer",
-    description: "Desarrollador web utilizando HTML, CSS, javascript y Python. Se realizó un sistema web para el control y la administración de los envases dentro del almacén.",
-    actual: false,
-  },
-  {
-    date: "",
     title: "Diputación Local del Estado de Veracruz, México. - Web Developer",
     description: "Desarrollador web utilizando HTML, CSS, javascript y Nodejs. Se realizó un sistema web para el control y la administración de la atención ciudadana.",
     actual: false,
   },
+  {
+    date: "",
+    title: "Super Tiendas Rarra's S.A. DE C.V. - Web Developer",
+    description: "Desarrollador web utilizando HTML, CSS, javascript y Python. Se realizó un sistema web para el control y la administración de los envases dentro del almacén.",
+    actual: false,
+  },
+  
 ];
 
 export const EXPERIENCES_EN = [
   {
-    date: "August 2021",
-    title: "Conekta - Sr Frontend Developer",
-    description: "Full-stack developer using frontend technologies such as React, Material UI, Chakra UI, TypeScript, and backend technologies like GO and Node.js. Conducted maintenance and support for the company's back-office application, along with the addition of new functionalities.",
+    date: "July 2021",
+    title: "Comercializadora de Cerverzas de los Tuxtlas S.A. DE C.V - Software Developer",
+    description: "Software Developer and DBA Support. Using C# .NET, Python and SQLServer as main technologies in the development of reporters and process automation.",
     actual: true,
   },
   {
-    date: "March 2021 - August 2021",
-    title: "Mercado Libre - Software Development SSr Analyst",
-    description: "Frontend Developer using HTML, CSS, TypeScript, React, and Node.js. Participated in the company's insurance application migration.",
+    date: "",
+    title: "Diputación Local del Estado de Veracruz, México. - Web Developer",
+    description: "Web developer using HTML, CSS, javascript and Nodejs. A web system was created for the control and administration of citizen attention.",
     actual: false,
   },
   {
-    date: "October 2018 - March 2021",
-    title: "Bancolombia - Software Engineer",
-    description: "Web, mobile, and service test automation using technologies such as Selenium, Appium, Rest Assured Screenplay, and Cucumber. Additionally, created automated tests for batch processes using AS400 libraries in JAVA. Led testing teams and defined testing strategies.",
+    date: "",
+    title: "Super Tiendas Rarra's S.A. DE C.V. - Web Developer",
+    description: "Web developer using HTML, CSS, javascript and Python. A web system was created to control and manage packaging within the warehouse.",
     actual: false,
   },
-  {
-    date: "November 2017 - September 2018",
-    title: "Accenture - Analyst",
-    description: "Web, mobile, and service test automation using technologies such as Selenium, Appium, Rest Assured Screenplay, and Cucumber.",
-    actual: false,
-  },
-  {
-    date: "August 2016 - November 2017",
-    title: "Tenebit - Frontend Developer",
-    description: "Web application developer using Ionic and AngularJs to manage/customize the end user's experience using a CRM.",
-    actual: false,
-  },
+  
 ];
 
 
